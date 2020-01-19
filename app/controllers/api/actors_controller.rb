13befc2cd04614627dbe838493f98dec9f63fead
mike <mikemcfaddin@gmail.com>
2020-01-19 16:09:35 -0500

    adds query, segment, and body request

--- app/controllers/api/actors_controller.rb
+++ app/controllers/api/actors_controller.rb
@@ -1,8 +1,14 @@
 class Api::ActorsController < ApplicationController
 
   def actor_action
-    @actor = Actor.all
+    @actors = Actor.all
     render "actor.json.jb"
+  end
+
+  def actor_search_action
+    person = params[:actor].capitalize
+    @actor = Actor.find_by(first_name: person)
+    render "actor_search.json.jb"
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,18 @@ Rails.application.routes.draw do
 
   # EXAMPLE JSON ROUTE WITH API NAMESPACE
   namespace :api do
-    get "/actor_url" => "actors#actor_action"
+
     get "/all_movies_url" => "movies#all_movies_action"
+
     get "/first_movie_url" => "movies#first_movie_action"
+
+
+    get "/actor_url" => "actors#actor_action"
+
+    get "/actor_query_url" => "actors#actor_search_action"
+
+    get "/actor_segment_url/:actor" => "actors#actor_search_action"
+
+    post "/actor_body_url" => "actors#actor_search_action"
   end
 end
