91f92b47b77d0cd7184319c519b387fa4b3effcb
mike <mikemcfaddin@gmail.com>
2020-01-30 20:54:59 -0500

    adds authentication to controllers

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -5,4 +5,5 @@ class Actor < ApplicationRecord
   validates :known_for, presence: true
   validates :age, numericality: { greater_than: 13}
   validates :first_name, :last_name, length: { minimum: 2 }
+  
 end
